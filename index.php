f2238f2a5409cd7d288dbd520cb6aee1aec6f81a
NicoDmwn <91961521+NicoDmwn@users.noreply.github.com>
2025-01-04 08:34:31 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+session_start();
+include_once("db_koneksi.php");
+
+if (!isset($_SESSION['username'])) {
+    header("Location: login.php");
+    exit();
+}
+?>
+
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Poliklinik</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+    
+</body>
+</html>
